eb9dfddf2c54ed57ba136f614f9c843c79d137dd
SunMi <wywudi@naver.com>
2024-08-07 13:45:54 +0900

    게이트 웨이

--- com.spring-cloud.eureka.second/src/main/java/com/spring_cloud/eureka/second/SecondController.java
+++ com.spring-cloud.eureka.second/src/main/java/com/spring_cloud/eureka/second/SecondController.java
@@ -4,21 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order")
 public class SecondController {
 
     @Autowired
     private final SecondService secondService;
 
-    @GetMapping("/order/{orderId}")
+    @GetMapping
+    public String getDefaultOrder(){
+        return "Order Default";
+    }
+
+    @GetMapping("/{orderId}")
     public String getOrder(@PathVariable String orderId) {
         return secondService.getOrder(orderId);
     }
 
-    @GetMapping("/order/details/{orderId}")
+    @GetMapping("/details/{orderId}")
     public Order getOrderDetails(@PathVariable String orderId) {
         return secondService.getOrderDetails(orderId);
     }
